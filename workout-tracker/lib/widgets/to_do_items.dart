bc0de95c64e8ef9865a6530486dedfdce1c3ddd1
David Krause <krausedl@hendrix.edu>
2026-09-10 18:43:40 -0500

    convert ExerciseListItem to stateful and add rep counter

--- workout-tracker/lib/widgets/to_do_items.dart
+++ workout-tracker/lib/widgets/to_do_items.dart
@@ -1,69 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:workout_tracker/objects/item.dart';
+import 'package:workout_tracker/objects/exercise.dart';
 
-typedef ToDoListChangedCallback = Function(Item item, bool completed);
-typedef ToDoListRemovedCallback = Function(Item item);
+typedef ExerciseRemovedCallback = Function(Exercise exercise);
 
-class ToDoListItem extends StatelessWidget {
-  ToDoListItem(
-      {required this.item,
-      required this.completed,
-      required this.onListChanged,
+class ExerciseListItem extends StatefulWidget {
+  ExerciseListItem(
+      {required this.exercise,
       required this.onDeleteItem})
-      : super(key: ObjectKey(item));
+      : super(key: ObjectKey(exercise));
 
-  final Item item;
-  final bool completed;
+      @override
+      State createState() => _ExerciseListItemState();
 
-  final ToDoListChangedCallback onListChanged;
-  final ToDoListRemovedCallback onDeleteItem;
+  final Exercise exercise;
+  
+  final ExerciseRemovedCallback onDeleteItem;
+}
 
-  Color _getColor(BuildContext context) {
-    // The theme depends on the BuildContext because different
-    // parts of the tree can have different themes.
-    // The BuildContext indicates where the build is
-    // taking place and therefore which theme to use.
-
-    return completed //
-        // should be the right place to change opacity to 54%
-        ? Colors.black54
-        : Theme.of(context).primaryColor;
-  }
-
-  // context looks like its here, so if we change the color from Colors.black -> Colors.black54 that should change the opacity
-  // but this is the text style, were looking for _getColor ^
-  TextStyle? _getTextStyle(BuildContext context) {
-    if (!completed) return null;
-
-    return const TextStyle(
-      // change opacity
-      // do not change opacity here
-      color: Colors.black,
-      decoration: TextDecoration.lineThrough,
-    );
-  }
-
+class _ExerciseListItemState extends State<ExerciseListItem> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {
-        onListChanged(item, completed);
+        setState(() {
+          widget.exercise.increment();
+        });
       },
-      onLongPress: completed
-          ? () {
-              onDeleteItem(item);
-            }
-          : null,
+      onLongPress: () {
+              widget.onDeleteItem(widget.exercise);
+            },
       leading: CircleAvatar(
-        // this is where we get the color
-        // but we are looking for aplha .5412 instead of alpha 1.0
-        // so look for context somewhere?
-        backgroundColor: _getColor(context),
-        child: Text(item.abbrev()),
+        child: Text(widget.exercise.abbrev()),
+      ),
+      trailing: Text(
+        widget.exercise.reps.toString()
       ),
       title: Text(
-        item.name,
-        style: _getTextStyle(context),
+        widget.exercise.name,
       ),
     );
   }
